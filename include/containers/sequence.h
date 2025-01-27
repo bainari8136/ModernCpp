b20d9363a4ea7a81fd3cd5e1008a5ee1ab7fbbd1
bainari8136 <williammsangazial@gmail.com>
2025-01-27 16:42:35 +0300

    update changes to the header file sequence.h

---
+++ include/containers/sequence.h
@@ -0,0 +1,17 @@
+
+#include <vector>
+
+#ifndef SEQUENCE_H
+#define SEQUENCE_H
+
+     namespace moderncpp {
+         
+         class SequenceContainers {
+             
+             public:
+             
+             void useIntVector(std::vector<int>vec);
+         };
+     }
+
+#endif
