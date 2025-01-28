8496756a36e1d8c55636c7da0c28e215fe11a12c
bainari8136 <williammsangazial@gmail.com>
2025-01-28 08:18:08 +0300

    update changes to the header file

---
+++ include/utils/container_utils.h
@@ -0,0 +1,17 @@
+#include <vector>
+
+#ifndef CONTAINER_UTILS_H
+#define CONTAINER_UTILS_H
+  
+  namespace moderncpp {
+	  
+	  class ContainerUtils {
+		  
+		  public:
+		  
+		  static void printVector(const std::vector<int>&vec);
+	  };
+  }
+
+
+#endif
